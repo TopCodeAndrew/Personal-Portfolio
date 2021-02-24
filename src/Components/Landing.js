ca93eb6535e9daeba24713f70366bebce2a4e445
Andrew Smith <topcodeandrew@gmail.com>
2021-02-24 11:41:51 -0700

    solved </ into arrow issue

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,15 +2,17 @@
 
 
 export default function Landing() {
-    let fakeH1 = '<h1>'
-    let fakeEndH1 = '< /h1>'
+    let fakeH1 = '< h1 >'
+    let fakeEndH1 = '< /h1 >'
     return (
         <div className="landing">
-            <div>
+            <div className='main-box'>
                 <p className='fakeCode'> {fakeH1}</p>
-                <h1>Hello</h1>
-                <h1>I'm</h1>
-                <h1>Andrew</h1>
+                <div className='big-letter-box'>
+                    <h1>Hello.</h1>
+                    <h1 className='red'>I'm</h1>
+                    <h1 className='red'>Andrew</h1>
+                </div>
                 <p className='fakeCode'> {fakeEndH1}</p>
             </div>
 
