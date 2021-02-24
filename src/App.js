6e7f57ab23e6d270276245bdd36527e2800f8592
Andrew Smith <topcodeandrew@gmail.com>
2021-02-23 19:26:11 -0700

    basic file structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './styling/App.css';
+import './styling/App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      test
+      <h1>Hello.</h1>
+      <h1>I'm</h1>
+      <h1>Andrew</h1>
+      <h2>test</h2>
+      <h3>test</h3>
     </div>
   );
 }
