b2b1d3b12a8a4d37ba4137207b2eb3dbfef53fe4
Andrew Smith <topcodeandrew@gmail.com>
2021-02-23 20:54:07 -0700

    basic landing style done

--- src/Components/ContactBar.js
+++ src/Components/ContactBar.js
@@ -7,9 +7,9 @@ import emailIcon from '../images/email-icon.png';
 export default function ContactBar() {
     return (
         <div className="contactBar">
-            <img src={githubIcon} />
-            <img src={emailIcon} />
             <img src={linkedInIcon} />
+            <img src={emailIcon} />
+            <img src={githubIcon} />
             <img src={spotifyIcon} />
         </div>
     );
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,9 +2,18 @@
 
 
 export default function Landing() {
+    let fakeH1 = '<h1>'
+    let fakeEndH1 = '< /h1>'
     return (
         <div className="landing">
-            <h1>Landing</h1>
-        </div>
+            <div>
+                <p className='fakeCode'> {fakeH1}</p>
+                <h1>Hello</h1>
+                <h1>I'm</h1>
+                <h1>Andrew</h1>
+                <p className='fakeCode'> {fakeEndH1}</p>
+            </div>
+
+        </div >
     );
 }
