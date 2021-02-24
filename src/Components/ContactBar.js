0141974e36aa0e4e2f56bd576e6acaa27e05ba0f
Andrew Smith <topcodeandrew@gmail.com>
2021-02-23 20:36:30 -0700

    basic sidebar layout done

--- src/Components/ContactBar.js
+++ src/Components/ContactBar.js
@@ -1,10 +1,16 @@
-
+import githubIcon from '../images/github-icon.png';
+import linkedInIcon from '../images/linkedIn-icon.png';
+import spotifyIcon from '../images/spotify-icon.png';
+import emailIcon from '../images/email-icon.png';
 
 
 export default function ContactBar() {
     return (
         <div className="contactBar">
-            <h1>ContactBar</h1>
+            <img src={githubIcon} />
+            <img src={emailIcon} />
+            <img src={linkedInIcon} />
+            <img src={spotifyIcon} />
         </div>
     );
 }
