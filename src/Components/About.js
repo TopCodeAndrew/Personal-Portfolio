595b60d3a9f9435aa21ec092d288d6b1cc34b5a4
Andrew Smith <topcodeandrew@gmail.com>
2021-02-25 23:31:18 -0700

    responsive work

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,10 +8,10 @@ export default function About() {
     let fakeEndP = '< /p >'
     return (
         <div className="about">
-            <div className='separating-line'></div>
+            {/* <div className='separating-line'></div>
             <div className='main-box'>
                 <p className='fakeCode'> {fakeH2}</p>
-                <h2>descriptors</h2>
+                <h2>des</h2>
                 <p className='fakeCode'> {fakeEndH2}</p>
             </div>
             <div className='content-box'>
@@ -20,7 +20,7 @@ export default function About() {
                     lorem Sit mollit reprehenderit cupidatat consequat adipisicing tempor est. Irure cillum incididunt labore incididunt dolore. Id excepteur excepteur qui consequat aliquip fugiat. Eiusmod ipsum id ut laboris quis sunt qui laborum.Non cillum pariatur culpa adipisicing. Commodo consectetur reprehenderit veniam adipisicing deserunt ut amet pariatur veniam laborum laboris voluptate. Culpa qui mollit ut sint magna aute et.
                 </p>
                 <p className='fakeCode'> {fakeEndP}</p>
-            </div>
+            </div> */}
         </div>
 
     );
