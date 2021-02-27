72884db274cc2a359407409316b2d6db43135cdd
Andrew Smith <topcodeandrew@gmail.com>
2021-02-27 10:59:30 -0700

    added scroll position state to app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Header from './Components/Header';
 import Landing from './Components/Landing';
 import SkillsExperience from './Components/SkillsExperience';
 import Footer from './Components/Footer';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <ContactBar />
       <Landing />
+      <button onClick={() => console.log(scrollPosition)}> Scroll Position</button>
       <About />
       <SkillsExperience />
       <Footer />
