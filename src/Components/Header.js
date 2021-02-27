08b1715524785b98fdcab191b1eb7aff78f28638
Andrew Smith <topcodeandrew@gmail.com>
2021-02-26 17:38:57 -0700

    start of header hamburger functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,33 @@
-
+import hamburger from '../images/hamburger.png'
 
 
 
 export default function Header() {
     return (
         <div className="header">
-            <span className='item-box'>
-                <p className='header-item'>Home</p>
-                <p className='header-item'>About</p>
-                <p className='header-item'>Skills/Experience</p>
-                <p className='header-item'>Contact</p>
-            </span>
+            <div className='desktop'>
+                <span className='item-box'>
+                    <p className='header-item'>Home</p>
+                    <p className='header-item'>About</p>
+                    <p className='header-item'>Skills/Experience</p>
+                    <p className='header-item'>Contact</p>
+                </span>
+            </div>
+            <div className='mobile'>
+                <img className='hamburger' src={hamburger} />
+                <div className='drop-down'>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Skills/Experience</p>
+                    <p>Contact</p>
+                </div>
+            </div>
         </div>
     );
 }
+
+//create the state to know if it should be visible
+//create a function that changes that state
+//create a function that returns 'show' if state is true, and 'hide' if state is false
+//put that function next to 'drop-down' in the drop-down div
+//in css, put the 'show' and 'hide' properties as a sibling of 'drop-down'
