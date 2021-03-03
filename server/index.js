da26c182e8cd376beda40ea67a6e58f54533262f
Andrew Smith <topcodeandrew@gmail.com>
2021-03-03 16:14:50 -0700

    working through hosting issues

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const SERVER_PORT = 3333
 
+const app = express();
+
 app.use(express.json())
 
 app.use(express.static(`${__dirname}/../build`));
