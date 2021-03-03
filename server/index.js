b3bd14f053bc9b7ec9f75c6f4d604822a59c30b3
Andrew Smith <topcodeandrew@gmail.com>
2021-03-03 16:12:57 -0700

    added listener

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
+const SERVER_PORT = 3333
 
 app.use(express.json())
 
 app.use(express.static(`${__dirname}/../build`));
 
-console.log('running')
+app.listen(SERVER_PORT, () => {
+    console.log(`Server jamming on port ${SERVER_PORT}`)
+})
